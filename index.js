04ea808c00bfa548e9cbdda7eb016e6b6d816c35
sanemat <o.gata.ken@gmail.com>
2015-07-22 04:07:23 +0900

    feat(remove): remove trailing separator

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 'use strict';
-module.exports = function (str, opts) {
-  if (typeof str !== 'string') {
-    throw new TypeError('Expected a string');
-  }
 
-  opts = opts || {};
-
-  return str + ' & ' + (opts.postfix || 'rainbows');
+module.exports = function (value) {
+  return value.replace(/[\\\/]+$/g, '');
 };
